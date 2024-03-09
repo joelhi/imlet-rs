ce0b7b90f4982c42b3e4d5e38b986ba43ec69192
joelhi <d.j.hilmersson@gmail.com>
2024-03-09 21:14:37 +0000

    Temp gyroid

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod marching_cubes;
 
-use std::time::Instant;
+use std::{fs, time::Instant};
 
 use marching_cubes::{algorithm::generate_iso_surface, types::{core::*, dense_grid::DenseGrid3f}};
 
-use crate::marching_cubes::types::implicit_functions::examples::DistanceFunction;
+use crate::marching_cubes::types::implicit_functions::examples::{DistanceFunction, GyroidFunction};
 
 fn main() {
     let grid = DenseGrid3f {
@@ -16,14 +16,34 @@ fn main() {
     };
 
     let distance_func = DistanceFunction{source: XYZ::get_origin()};
+    let gyroid = GyroidFunction{
+        length_x: 5.0,
+        length_y: 5.0,
+        length_z: 5.0
+    };
 
-    let (coord, values) = grid.evaluate(&distance_func);
+    let before = Instant::now();
+    let (coord, values) = grid.evaluate(&gyroid);
 
-    let triangles = generate_iso_surface(grid, 5.0, &coord, &values);
+    let triangles = generate_iso_surface(grid, 0.0, &coord, &values);
 
-    for t in triangles{
-        println!("{{{},{},{}}}",t.p1.x,t.p1.y,t.p1.z);
-        println!("{{{},{},{}}}",t.p2.x,t.p2.y,t.p2.z);
-        println!("{{{},{},{}}}",t.p3.x,t.p3.y,t.p3.z);
-    }
+    println!("Isosurface for {} points generated in {:.2?}", grid.get_size(), before.elapsed());
+
+    fs::write("data.txt", get_triangle_as_str(&triangles)).expect("Unable to write file");
+
+}
+
+fn get_triangle_as_str(triangles: &Vec<Triangle>)->String{
+    let mut data = String::new();
+
+        for t in triangles{
+            let p1 = format!("{{{},{},{}}}\n",t.p1.x,t.p1.y,t.p1.z);
+            data.push_str(&p1);
+            let p2 = format!("{{{},{},{}}}\n",t.p2.x,t.p2.y,t.p2.z);
+            data.push_str(&p2);
+            let p3 = format!("{{{},{},{}}}\n",t.p3.x,t.p3.y,t.p3.z);
+            data.push_str(&p3);
+        }
+
+        data
 }
--- src/marching_cubes/types/implicit_functions/examples.rs
+++ src/marching_cubes/types/implicit_functions/examples.rs
@@ -1,22 +1,28 @@
-use crate::marching_cubes::types::core::XYZ;
+use std::f32::consts::PI;
+
 use super::base::ImplicitFunction;
+use crate::marching_cubes::types::core::XYZ;
 
-pub struct GyroidFunction{
-
+pub struct GyroidFunction {
+    pub length_x: f32,
+    pub length_y: f32,
+    pub length_z: f32,
 }
 
-impl ImplicitFunction for GyroidFunction{
-    fn eval(&self, pt:XYZ)->f32{
-            0.0
+impl ImplicitFunction for GyroidFunction {
+    fn eval(&self, pt: XYZ) -> f32 {
+        (2.0 * PI * pt.x / self.length_x).sin() * (2.0 * PI * pt.y / self.length_y).cos()
+            + (2.0 * PI * pt.y / self.length_y).sin() * (2.0 * PI * pt.z / self.length_z).cos()
+            + (2.0 * PI * pt.z / self.length_z).sin() * (2.0 * PI * pt.x / self.length_x).cos()
     }
 }
 
-pub struct DistanceFunction{
-    pub source:XYZ
+pub struct DistanceFunction {
+    pub source: XYZ,
 }
 
-impl ImplicitFunction for DistanceFunction{
-    fn eval(&self, pt:XYZ)->f32{
-            self.source.distance_to(pt)
+impl ImplicitFunction for DistanceFunction {
+    fn eval(&self, pt: XYZ) -> f32 {
+        self.source.distance_to(pt)
     }
 }
