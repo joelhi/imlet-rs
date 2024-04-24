9dd078fc8e2b80140ea374ce9bd83881362b07ca
joelhi <d.j.hilmersson@gmail.com>
2024-04-24 18:09:02 +0100

    Fix plane

--- src/engine/types/functions/clipping.rs
+++ src/engine/types/functions/clipping.rs
@@ -11,9 +11,8 @@ pub struct ClippingPlane<T: ImplicitFunction>{
 
 impl<T: ImplicitFunction> ImplicitFunction for ClippingPlane<T>{
     fn eval(&self, x: f32, y: f32, z: f32) -> f32 {
-        match self.plane.signed_distance(XYZ::new(x, y, z)){
-            val if val < 0.0 => 0.0,
-            _ => self.function.eval(x, y, z),
-        }
+        let plane_iso = self.plane.signed_distance(XYZ::new(x, y, z));
+        let val = self.function.eval(x, y, z);
+        plane_iso.max(val)
     }
 }
