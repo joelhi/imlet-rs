5db7a732e25ff0177269f990a826d228b004fb1d
joelhi <d.j.hilmersson@gmail.com>
2024-05-18 17:14:47 +0100

    Update visibility of component enum

--- src/engine/types/computation/mod.rs
+++ src/engine/types/computation/mod.rs
@@ -1,9 +1,9 @@
 mod component;
+mod dense_field;
 pub mod distance_functions;
 mod model;
 pub mod operations;
-mod dense_field;
 
+use component::*;
 pub use dense_field::*;
-pub use component::*;
 pub use model::*;
