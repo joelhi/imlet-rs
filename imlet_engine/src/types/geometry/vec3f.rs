45f28c992eead82c59d7ad90fa738ce4018b86ff
joelhi <d.j.hilmersson@gmail.com>
2024-06-02 13:49:38 +0100

    Make vec3 generic

--- imlet_engine/src/types/geometry/vec3f.rs
+++ imlet_engine/src/types/geometry/vec3f.rs
@@ -1,103 +1,105 @@
-use std::ops;
+use std::{fmt::Debug, ops};
+
+use num_traits::Float;
 
 #[derive(Debug, Clone, Copy)]
-pub struct Vec3f {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+pub struct Vec3<T> where T: Float + Debug {
+    pub x: T,
+    pub y: T,
+    pub z: T,
 }
 
-impl Vec3f {
-    pub fn new(x: f32, y: f32, z: f32) -> Self {
-        Vec3f { x: x, y: y, z: z }
+impl<T: Float + Debug> Vec3<T> {
+    pub fn new(x: T, y: T, z: T) -> Self {
+        Self { x: x, y: y, z: z }
     }
 
-    pub fn distance_to_vec3f(&self, pt: Vec3f) -> f32 {
-        self.distance_to_vec3f_squared(pt).sqrt()
+    pub fn distance_to_vec3(&self, pt: Vec3<T>) -> T {
+        self.distance_to_vec3_squared(pt).sqrt()
     }
 
-    pub fn distance_to_coord(&self, x: f32, y: f32, z: f32) -> f32 {
+    pub fn distance_to_coord(&self, x: T, y: T, z: T) -> T {
         self.distance_to_coord_squared(x, y, z).sqrt()
     }
 
-    pub fn distance_to_vec3f_squared(&self, pt: Vec3f) -> f32 {
+    pub fn distance_to_vec3_squared(&self, pt: Vec3<T>) -> T {
         self.distance_to_coord_squared(pt.x, pt.y, pt.z)
     }
 
-    pub fn distance_to_coord_squared(&self, x: f32, y: f32, z: f32) -> f32 {
+    pub fn distance_to_coord_squared(&self, x: T, y: T, z: T) -> T {
         (self.x - x).powi(2) + (self.y - y).powi(2) + (self.z - z).powi(2)
     }
 
-    pub fn origin() -> Vec3f {
-        Vec3f {
+    pub fn origin() -> Vec3<T> {
+        Self {
             x: 0.0,
             y: 0.0,
             z: 0.0,
         }
     }
 
-    pub fn x_axis() -> Vec3f {
-        Vec3f {
+    pub fn x_axis() -> Vec3<T> {
+        Self {
             x: 1.0,
             y: 0.0,
             z: 0.0,
         }
     }
 
-    pub fn y_axis() -> Vec3f {
-        Vec3f {
+    pub fn y_axis() -> Vec3<T> {
+        Self {
             x: 0.0,
             y: 1.0,
             z: 0.0,
         }
     }
 
-    pub fn z_axis() -> Vec3f {
-        Vec3f {
+    pub fn z_axis() -> Vec3<T> {
+        Self {
             x: 0.0,
             y: 0.0,
             z: 1.0,
         }
     }
 
-    pub fn interpolate(first: &Vec3f, second: &Vec3f, parameter: f32) -> Vec3f {
-        Vec3f {
+    pub fn interpolate(first: &Vec3<T>, second: &Vec3<T>, parameter: T) -> Vec3<T> {
+        Self{
             x: first.x + parameter * (second.x - first.x),
             y: first.y + parameter * (second.y - first.y),
             z: first.z + parameter * (second.z - first.z),
         }
     }
 
-    pub fn dot(&self, rhs: Vec3f) -> f32 {
+    pub fn dot(&self, rhs: Vec3<T>) -> T {
         (self.x * rhs.x) + (self.y * rhs.y) + (self.z * rhs.z)
     }
 
-    pub fn dot_vec3f(&self, x: f32, y: f32, z: f32) -> f32 {
+    pub fn dot_vec3(&self, x: T, y: T, z: T) -> T {
         (self.x * x) + (self.y * y) + (self.z * z)
     }
 
-    pub fn cross(&self, rhs: Vec3f) -> Vec3f {
-        Vec3f {
+    pub fn cross(&self, rhs: Vec3<T>) -> Vec3<T> {
+        Self {
             x: self.y * rhs.z - self.z * rhs.y,
             y: self.z * rhs.x - self.x * rhs.z,
             z: self.x * rhs.y - self.y * rhs.x,
         }
     }
 
-    pub fn magnitude(&self) -> f32 {
+    pub fn magnitude(&self) -> T {
         self.distance_to_coord(0.0, 0.0, 0.0)
     }
 
-    pub fn normalize(&self) -> Vec3f {
+    pub fn normalize(&self) -> Vec3<T> {
         *self / self.magnitude()
     }
 }
 
-impl ops::Add<Vec3f> for Vec3f {
-    type Output = Vec3f;
-    fn add(self, _rhs: Vec3f) -> Vec3f {
+impl<T: Float + Debug> ops::Add<Vec3<T>> for Vec3<T> {
+    type Output = Vec3<T>;
+    fn add(self, _rhs: Vec3<T>) -> Vec3<T> {
         {
-            Vec3f {
+            Self {
                 x: self.x + _rhs.x,
                 y: self.y + _rhs.y,
                 z: self.z + _rhs.z,
@@ -106,10 +108,10 @@ impl ops::Add<Vec3f> for Vec3f {
     }
 }
 
-impl ops::Sub<Vec3f> for Vec3f {
-    type Output = Vec3f;
-    fn sub(self, _rhs: Vec3f) -> Vec3f {
-        Vec3f {
+impl<T: Float + Debug> ops::Sub<Vec3<T>> for Vec3<T> {
+    type Output = Vec3<T>;
+    fn sub(self, _rhs: Vec3<T>) -> Vec3<T> {
+        Self {
             x: self.x - _rhs.x,
             y: self.y - _rhs.y,
             z: self.z - _rhs.z,
@@ -117,10 +119,10 @@ impl ops::Sub<Vec3f> for Vec3f {
     }
 }
 
-impl ops::Mul<f32> for Vec3f {
-    type Output = Vec3f;
-    fn mul(self, rhs: f32) -> Self::Output {
-        Vec3f {
+impl<T: Float + Debug> ops::Mul<T> for Vec3<T> {
+    type Output = Vec3<T>;
+    fn mul(self, rhs: T) -> Self::Output {
+        Self {
             x: self.x * rhs,
             y: self.y * rhs,
             z: self.z * rhs,
@@ -128,31 +130,9 @@ impl ops::Mul<f32> for Vec3f {
     }
 }
 
-impl ops::Mul<Vec3f> for f32 {
-    type Output = Vec3f;
-    fn mul(self, rhs: Vec3f) -> Self::Output {
-        Vec3f {
-            x: self * rhs.x,
-            y: self * rhs.y,
-            z: self * rhs.z,
-        }
-    }
-}
-
-impl ops::Mul<Vec3f> for Vec3f {
-    type Output = f32;
-    fn mul(self, rhs: Vec3f) -> Self::Output {
+impl<T: Float + Debug> ops::Mul<Vec3<T>> for Vec3<T> {
+    type Output = T;
+    fn mul(self, rhs: Vec3<T>) -> Self::Output {
         self.dot(rhs)
-    }
-}
-
-impl ops::Div<f32> for Vec3f {
-    type Output = Vec3f;
-    fn div(self, rhs: f32) -> Self::Output {
-        Vec3f {
-            x: self.x / rhs,
-            y: self.y / rhs,
-            z: self.z / rhs,
-        }
     }
 }
