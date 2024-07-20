fd0f03f78d6f4e72318f1ddc0946f727aac244b4
joelhi <d.j.hilmersson@gmail.com>
2024-07-20 11:32:07 +0100

    Traits for spatial query

--- engine/src/types/geometry/traits/signed_distance.rs
+++ engine/src/types/geometry/traits/signed_distance.rs
@@ -1,3 +1,7 @@
+use std::fmt::Debug;
+
+use num_traits::Float;
+
 pub trait SignedDistance<T: Float + Debug + Send + Sync>: Sync + Send {
     fn signed_distance(&self, x: T, y: T, z: T) -> T;
 }
--- engine/src/types/geometry/traits/spatial_query.rs
+++ engine/src/types/geometry/traits/spatial_query.rs
@@ -1,5 +1,17 @@
-pub trait ImplicitFunction<T: Float + Debug + Send + Sync>: Sync + Send {
+use std::fmt::Debug;
+
+use num_traits::Float;
+
+use crate::types::geometry::{BoundingBox, Vec3};
+
+pub trait SpatialQuery<T: Float + Debug + Send + Sync>: Copy {
     fn bounds(&self) -> BoundingBox<T>;
 
-    fn closet_point(&self, query: Vec3<T>)->Vec3<T>;
+    fn closest_point(&self, query_point: &Vec3<T>) -> Vec3<T>;
+
+    fn default() -> Self;
+}
+
+pub trait SignedQuery<T: Float + Debug + Send + Sync>: SpatialQuery<T> {
+    fn normal_at(&self, query_point: &Vec3<T>) -> Vec3<T>;
 }
