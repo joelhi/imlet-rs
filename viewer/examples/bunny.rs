4ec11328ab799feaf4f7bcaebf39ef840a8a3403
joelhi <d.j.hilmersson@gmail.com>
2024-09-16 18:08:08 +0100

    Update bunny example

--- viewer/examples/bunny.rs
+++ viewer/examples/bunny.rs
@@ -16,14 +16,14 @@ use {
 pub fn main() {
     utils::logging::init_info();
 
-    let mesh: Mesh<f64> = parse_obj_file("assets/geometry/bunny.obj").unwrap();
+    let mesh: Mesh<f64> = parse_obj_file("assets/geometry/bunny.obj", false).unwrap();
 
     let cell_size = 0.5;
     let model_space = mesh.bounds().offset(cell_size);
 
     // Build model
     let mut model = ImplicitModel::new();
-    model.add_function("BunnyMesh", MeshSDF::new(&mesh, 10, 12, 0.1));
+    model.add_function("BunnyMesh", MeshSDF::new(&mesh, 10, 12));
     model.add_function("GyroidInfill", Gyroid::with_equal_spacing(7.5, true));
     model.add_operation_with_inputs("ThickGyroid", Thickness::new(5.0), &vec!["GyroidInfill"]);
     model.add_operation_with_inputs(
