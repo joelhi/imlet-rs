5d4d8d73d353965fcb134720f7d18e8c61842872
joelhi <d.j.hilmersson@gmail.com>
2024-09-23 18:24:11 +0100

    more model errors

--- engine/src/types/computation/model_error.rs
+++ engine/src/types/computation/model_error.rs
@@ -1,17 +1,61 @@
 #[derive(Debug)]
 pub enum ModelError {
-    InvalidInput { component: String, dependency: String },
-    RemoveError(String),
-    // Add more variants as needed
+    CyclicDependency(String),
+    MissingTag(String),
+    DuplicateTag(String),
+    InputIndexOutOfRange {
+        component: String,
+        num_inputs: usize,
+        index: usize,
+    },
+    IncorrectInputCount {
+        component: String,
+        num_inputs: usize,
+        count: usize,
+    },
+    MissingInput {
+        component: String,
+        index: usize,
+    },
 }
 
 impl std::fmt::Display for ModelError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ModelError::InvalidInput { component, dependency } => {
-                write!(f, "Invalid input component {} specified for {}. The component will depend on itself.", component, dependency)
+            ModelError::CyclicDependency(component) => {
+                write!(
+                    f,
+                    "Cyclic dependency detected for {}. The component is depending on itself.",
+                    component
+                )
             }
-            ModelError::RemoveError(msg) => write!(f, "Remove error: {}", msg),
+            ModelError::MissingTag(tag) => {
+                write!(f, "Could not find component with tag {} in model.", tag)
+            }
+            ModelError::DuplicateTag(tag) => {
+                write!(f, "Component with tag {} already present in model.", tag)
+            }
+            ModelError::InputIndexOutOfRange {
+                component,
+                num_inputs,
+                index,
+            } => {
+                write!(f, "Input index out of bounds for component {}. The recieved index ({}) is larget than the input count for the component ({})", component, index, num_inputs)
+            }
+            ModelError::IncorrectInputCount {
+                component,
+                num_inputs,
+                count,
+            } => {
+                write!(f, "Incorrect inputs for component {}. The recieved number ({}) is larget than the input count for the component ({})", component, count, num_inputs)
+            }
+            ModelError::MissingInput { component, index } => {
+                write!(
+                    f,
+                    "Component {} is missing an input at index {}. The model cannot be computed.",
+                    component, index
+                )
+            }
         }
     }
 }
