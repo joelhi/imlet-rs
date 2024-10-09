5c0303264b267a473dbe0c3b6d33bb2a25961294
joelhi <d.j.hilmersson@gmail.com>
2024-10-09 18:25:32 +0100

    Formatting

--- src/viewer/app.rs
+++ src/viewer/app.rs
@@ -3,13 +3,11 @@ use bevy::app::App;
 use super::plugins::MeshViewerPlugin;
 
 /// Open an interactive window which renders a mesh object with orbit controls.
-/// 
+///
 /// # Arguments
 ///
 /// * `mesh` - The mesh to render.
 ///
 pub fn show_mesh(mesh: &crate::types::geometry::Mesh<f32>) {
-    App::new()
-        .add_plugins(MeshViewerPlugin::new(mesh))
-        .run();
+    App::new().add_plugins(MeshViewerPlugin::new(mesh)).run();
 }
