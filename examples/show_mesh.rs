e67444bbd54d53a62302c47b3df72e49fa76cc8d
joelhi <d.j.hilmersson@gmail.com>
2025-01-15 17:53:16 +0000

    Polish examples

--- examples/show_mesh.rs
+++ examples/show_mesh.rs
@@ -1,11 +1,14 @@
 use imlet::utils;
 
 pub fn main() {
-    utils::logging::init_info();
+    env_logger::init();
     #[cfg(feature = "viewer")]
     {
         let mesh: imlet::types::geometry::Mesh<f32> =
             utils::io::parse_obj_file("assets/geometry/bunny.obj", false, false).unwrap();
         imlet::viewer::show_mesh(&mesh, Some(mesh.bounds()));
+    }
+    #[cfg(not(feature = "viewer"))]{
+        log::error!("Enable the viewer feature to see the model.");
     }
 }
